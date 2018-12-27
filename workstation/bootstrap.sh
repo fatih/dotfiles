87d9e7d8d2f09101620e4fba7eb7bd81526b3b2f
Fatih Arslan <ftharsln@gmail.com>
2018-12-28 00:40:15 +0300

    Latest tmux changes

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -235,6 +235,7 @@ cd /home/fatih/code
 # tmux plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tmux-open.git ~/.tmux/plugins/tmux-open
+git clone https://github.com/tmux-plugins/tmux-yank.git ~/.tmux/plugins/tmux-yank
 git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/plugins/tmux-prefix-highlight
 
 git clone --recursive https://github.com/fatih/dotfiles.git  && cd dotfiles
