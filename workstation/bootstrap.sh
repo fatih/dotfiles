6ef968b0ce713114ba1a520221aae76af857906c
Fatih Arslan <ftharsln@gmail.com>
2018-12-31 11:16:13 +0300

    Latest changes

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -12,15 +12,16 @@ apt-get install -qq -y \
   git \
   jq \
   mosh \
-	--no-install-recommends
+  curl \
+  unzip \
+  openssh-server
 
-chsh -s /usr/bin/zsh
+echo "Creating directories"
+
+mkdir -p /mnt/code /mnt/secrets
 
 echo "Installing 1password"
 curl -sS -o 1password.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.5.4/op_linux_amd64_v0.5.4.zip && unzip 1password.zip op -d /usr/local/bin && rm 1password.zip
-
-echo "Pulling fatih/dev"
-docker pull fatih/dev
 
 echo "=> Setting up dev service"
 cat > dockerdev.service <<EOF
@@ -34,7 +35,7 @@ TimeoutStartSec=0
 ExecStartPre=-/usr/bin/docker kill dev
 ExecStartPre=-/usr/bin/docker rm dev
 ExecStartPre=/usr/bin/docker pull fatih/dev
-ExecStart=/usr/bin/docker run -h dev --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /home/fatih/code:/home/fatih/code -v /home/fatih/.ssh:/home/fatih/.ssh -v /home/fatih/.zsh_private:/home/fatih/.zsh_private -v /home/fatih/.zsh_history:/home/fatih/.zsh_history --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev fatih/dev
+ExecStart=/usr/bin/docker run -h dev --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/mnt/code -v /mnt/secrets:/home/fatih/secrets -v  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev fatih/dev
 
 [Install]
 WantedBy=multi-user.target
@@ -44,6 +45,5 @@ sudo mv dockerdev.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable dockerdev
 sudo systemctl start dockerdev
-
 
 echo "Done!"
