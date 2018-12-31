a7b202e28790468e1ba8ab25aeb2758a6241ab9b
Fatih Arslan <ftharsln@gmail.com>
2019-01-01 01:19:25 +0300

    Fix timezone

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -35,7 +35,7 @@ TimeoutStartSec=0
 ExecStartPre=-/usr/bin/docker kill dev
 ExecStartPre=-/usr/bin/docker rm dev
 ExecStartPre=/usr/bin/docker pull fatih/dev
-ExecStart=/usr/bin/docker run -h dev --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets -v  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev fatih/dev
+ExecStart=/usr/bin/docker run -h dev -e TZ=Europe/Istanbul --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets -v  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev fatih/dev
 
 [Install]
 WantedBy=multi-user.target
