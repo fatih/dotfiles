011429416819201ecf3b5d6a76507669c2e9d4cb
Fatih Arslan <ftharsln@gmail.com>
2019-01-03 01:20:34 +0300

    Update entrypoint.sh

--- workstation/entrypoint.sh
+++ workstation/entrypoint.sh
@@ -8,18 +8,19 @@ if [ ! -d ~/code/dotfiles ]; then
   # if needed
   cd ~/code
   git clone --recursive https://github.com/fatih/dotfiles.git
-  cd dotfiles 
-  git remote set-url origin git@github.com:fatih/dotfiles.git
-  
-  ln -s $(pwd)/vimrc ~/.vimrc
-  ln -s $(pwd)/zshrc ~/.zshrc
-  ln -s $(pwd)/tmuxconf ~/.tmux.conf
-  ln -s $(pwd)/tigrc ~/.tigrc
-  ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
-  ln -s $(pwd)/gitconfig ~/.gitconfig
-  ln -s $(pwd)/agignore ~/.agignore
-  ln -s $(pwd)/sshconfig ~/.ssh/config
 fi
+
+cd ~/code/dotfiles 
+git remote set-url origin git@github.com:fatih/dotfiles.git
+
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/tmuxconf ~/.tmux.conf
+ln -s $(pwd)/tigrc ~/.tigrc
+ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
+ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/agignore ~/.agignore
+ln -s $(pwd)/sshconfig ~/.ssh/config
 
 
 /usr/sbin/sshd -D
