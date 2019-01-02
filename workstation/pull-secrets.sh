6431703259a36b962b09052d62fdc8f67b8837aa
Fatih Arslan <ftharsln@gmail.com>
2019-01-03 01:08:28 +0300

    Update pull-secrets.sh

--- workstation/pull-secrets.sh
+++ workstation/pull-secrets.sh
@@ -11,6 +11,10 @@ op get document 'github_rsa' > github_rsa
 op get document 'zsh_private' > zsh_private
 op get document 'zsh_history' > zsh_history
 
+rm ~/.ssh/github_rsa
+rm ~/.zsh_private
+rm ~/.zsh_history
+
 ln -s $(pwd)/github_rsa ~/.ssh/github_rsa
 chmod 0600 ~/.ssh/github_rsa
 ln -s $(pwd)/zsh_private ~/.zsh_private
