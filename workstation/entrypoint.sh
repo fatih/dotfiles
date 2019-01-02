5a41b0e6487cfbcfd6b7a737a11c472a016859ce
Fatih Arslan <ftharsln@gmail.com>
2019-01-03 01:05:25 +0300

    Change the way we clone dotfiles

---
+++ workstation/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# the reason we dont't copy the files individually is, to easily push changes
+# if needed
+cd ~/code
+git clone --recursive https://github.com/fatih/dotfiles.git
+cd dotfiles 
+git remote set-url origin git@github.com:fatih/dotfiles.git
+
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/tmuxconf ~/.tmux.conf
+ln -s $(pwd)/tigrc ~/.tigrc
+ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
+ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/agignore ~/.agignore
+ln -s $(pwd)/sshconfig ~/.ssh/config
+
+
+/usr/sbin/sshd -D
+
