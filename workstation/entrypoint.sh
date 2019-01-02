82050b7443f44c355e28e77bd4728ccaad2227bb
Fatih Arslan <ftharsln@gmail.com>
2019-01-03 01:12:23 +0300

    Fix dotfiles

--- workstation/entrypoint.sh
+++ workstation/entrypoint.sh
@@ -2,21 +2,24 @@
 
 set -e
 
-# the reason we dont't copy the files individually is, to easily push changes
-# if needed
-cd ~/code
-git clone --recursive https://github.com/fatih/dotfiles.git
-cd dotfiles 
-git remote set-url origin git@github.com:fatih/dotfiles.git
-
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/zshrc ~/.zshrc
-ln -s $(pwd)/tmuxconf ~/.tmux.conf
-ln -s $(pwd)/tigrc ~/.tigrc
-ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
-ln -s $(pwd)/gitconfig ~/.gitconfig
-ln -s $(pwd)/agignore ~/.agignore
-ln -s $(pwd)/sshconfig ~/.ssh/config
+if [ ! -d ~/code/dotfiles ]; then
+  echo "Cloning dotfiles"
+  # the reason we dont't copy the files individually is, to easily push changes
+  # if needed
+  cd ~/code
+  git clone --recursive https://github.com/fatih/dotfiles.git
+  cd dotfiles 
+  git remote set-url origin git@github.com:fatih/dotfiles.git
+  
+  ln -s $(pwd)/vimrc ~/.vimrc
+  ln -s $(pwd)/zshrc ~/.zshrc
+  ln -s $(pwd)/tmuxconf ~/.tmux.conf
+  ln -s $(pwd)/tigrc ~/.tigrc
+  ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
+  ln -s $(pwd)/gitconfig ~/.gitconfig
+  ln -s $(pwd)/agignore ~/.agignore
+  ln -s $(pwd)/sshconfig ~/.ssh/config
+fi
 
 
 /usr/sbin/sshd -D
