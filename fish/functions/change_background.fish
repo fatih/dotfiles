abb1fd01cfa094808cad5ba1bdf37aa154c1c528
Fatih Arslan <ftharsln@gmail.com>
2021-02-07 13:58:31 +0300

    Change background for a given argument

--- fish/functions/change_background.fish
+++ fish/functions/change_background.fish
@@ -1,15 +1,35 @@
-function change_background --argument mode
+function change_background --argument mode_setting
+  # change background to the given mode. If mode is missing, 
+  # we try to deduct it from the system settings.
+  #
+
+  set -l mode "light" # default value
+  if test -z $mode_setting
+    set -l val (defaults read -g AppleInterfaceStyle) >/dev/null
+    if test $status -eq 0
+      set mode "dark"
+    end
+  else
+    switch $mode_setting
+      case light
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" >/dev/null
+        set mode "light"
+      case dark
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" >/dev/null
+        set mode "dark"
+    end
+  end
+
   for pid in (pgrep vim)
     # vim automatically reloads changes the background once it receives a SIGUSR1
     kill -SIGUSR1 $pid
   end
 
-
-  set -l mode (defaults read -g AppleInterfaceStyle) >/dev/null
-  if test $status -eq 0
-        alacritty-theme gruvbox_dark
-  else
-        alacritty-theme gruvbox_light
+  switch $mode
+    case dark
+      alacritty-theme gruvbox_dark
+    case light
+      alacritty-theme gruvbox_light
   end
 
   # todo: TMUX colors
