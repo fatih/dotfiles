45e7fe65b75092c3be0e28fdd2cb9eec92c50bce
Fatih Arslan <ftharsln@gmail.com>
2021-06-28 11:07:17 +0300

    Latest updates

--- config.fish
+++ config.fish
@@ -34,3 +34,5 @@ status --is-interactive; and source (rbenv init -|psub)
 source ~/.private.fish
 
 set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
+
