e210ba69e0946847b49b6875466535b20d14eba2
Fatih Arslan <ftharsln@gmail.com>
2021-07-31 12:24:45 +0300

    Add layers and make the leds shine

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -1,6 +1,17 @@
 #include QMK_KEYBOARD_H
 
-#define QWERTY 0 // Base qwerty
+enum layer_names {
+    _QWERTY,
+    _SYMBOLS,
+    _NUMPAD
+};
+
+// Mac-specific macros
+#define MACCOPY LGUI(KC_C)
+#define MACPAST LGUI(KC_V)
+#define MACUNDO LGUI(KC_Z)
+#define MACREDO LGUI(KC_Y)
+#define MACLOCK LGUI(LCTL(KC_Q))
 
 
 /****************************************************************************************************
@@ -31,7 +42,8 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[QWERTY] = LAYOUT(
+[_QWERTY] = LAYOUT(
+
            KC_ESC, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
@@ -40,8 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,
 			   KC_LGUI,KC_LALT,
                                     KC_HOME,
-                           KC_BSPC,KC_DEL ,KC_END ,
-    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SLCK  ,KC_PAUS, KC_FN0, RESET,
+                           KC_BSPC, MO(_SYMBOLS),KC_END ,
+
+    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,MACLOCK  ,KC_PAUS, KC_FN0, RESET,
 	KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
 	KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
 	KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,RCTL_T(KC_QUOT),
@@ -49,8 +62,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
            KC_RALT,RGUI_T(KC_ESC),
            KC_PGUP,
-           KC_PGDN,KC_ENTER ,KC_SPC
-    )
+           MO(_NUMPAD),KC_ENTER ,KC_SPC
+    ),
+
+[_SYMBOLS] = LAYOUT(
+         _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,
+                   _______,  _______,  _______,  _______,
+                             _______,  _______,
+                                       _______,
+                    _______, _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  KC_ASTERISK,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+                   _______,  _______,  _______,  _______,
+         _______,  _______,
+         _______,
+         _______,  _______,  _______
+    ),
+
+[_NUMPAD] = LAYOUT(
+         _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,
+                   _______,  _______,  _______,  _______,
+                             _______,  _______,
+                                       _______,
+                    _______, _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+         _______,  _______,  _______,  _______,  _______,  _______,
+                   _______,  _______,  _______,  _______,
+         _______,  _______,
+         _______,
+         _______,  _______,  _______
+    ),
 };
 
 void matrix_init_user(void) {
@@ -67,4 +122,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void led_set_user(uint8_t usb_led) {
 
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _SYMBOLS:
+        writePinLow(LED_NUM_LOCK_PIN);
+        break;
+    case _NUMPAD:
+        writePinLow(LED_SCROLL_LOCK_PIN);
+        break;
+    default: //  for any other layers, or the default layer
+        writePinLow(LED_CAPS_LOCK_PIN);
+        writePinHigh(LED_NUM_LOCK_PIN);
+        writePinHigh(LED_SCROLL_LOCK_PIN);
+        writePinHigh(LED_COMPOSE_PIN);
+        break;
+    }
+  return state;
+}
+
+bool led_update_user(led_t led_state) {
+    // Disable led_update_kb() so that layer indication code doesn't get overridden.
+    return false;
 }
