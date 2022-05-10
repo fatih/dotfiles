9260ec3d85bc32db8e1cf5e6509c31799335df2d
Fatih Arslan <ftharsln@gmail.com>
2022-05-10 11:38:35 +0300

    Fix usego function

--- fish/functions/usego.fish
+++ fish/functions/usego.fish
@@ -7,11 +7,11 @@ function usego --argument ver
   set go_bin_path (command -v "go$ver")
   if test -z $go_bin_path
     echo "go $ver does not exist, installing with commands: "
-    echo "  go install golang.org/dl/go$ver"
+    echo "  go install golang.org/dl/go$ver@latest"
     echo "  go$ver download"
     echo ""
 
-    go get "golang.org/dl/go$ver"
+    go install "golang.org/dl/go$ver@latest"
     go$ver download
     set go_bin_path (command -v "go$ver")
   end
