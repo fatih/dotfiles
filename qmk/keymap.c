9c60ab7c670e90cd707810b5615eff325736d496
Fatih Arslan <ftharsln@gmail.com>
2022-06-16 10:22:45 +0300

    Use / instead of enter in numpad mode

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______,  _______,  KC_KP_EQUAL,  KC_KP_SLASH,  KC_KP_ASTERISK,  _______,
          _______,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_MINUS,  _______,
          _______,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_PLUS,  _______,
-         _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_ENTER,  _______,
+         _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_SLSH,  _______,
                    _______,  _______,  KC_KP_DOT,  KC_KP_ENTER,
          _______,  _______,
          _______,
