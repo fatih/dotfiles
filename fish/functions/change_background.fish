11f36b26914cded29e0e85db81996115bf7ced5d
Fatih Arslan <ftharsln@gmail.com>
2023-04-29 01:10:07 +0300

    Fix change-background for neovim
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- fish/functions/change_background.fish
+++ fish/functions/change_background.fish
@@ -19,16 +19,13 @@ function change_background --argument mode_setting
     end
   end
 
-  # well, seems like there is no proper way to send a command to 
-  # Vim as a client. Luckily we're using tmux, which means we can 
-  # iterate over all vim sessions and change the background ourself.
-  set -l tmux_wins (/opt/homebrew/bin/tmux list-windows -t main)
-
-  for wix in (/opt/homebrew/bin/tmux list-windows -t main -F 'main:#{window_index}')
-    for pix in (/opt/homebrew/bin/tmux list-panes -F 'main:#{window_index}.#{pane_index}' -t $wix)
-      set -l is_vim "ps -o state= -o comm= -t '#{pane_tty}'  | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?\$'"
-      /opt/homebrew/bin/tmux if-shell -t "$pix" "$is_vim" "send-keys -t $pix escape ENTER"
-      /opt/homebrew/bin/tmux if-shell -t "$pix" "$is_vim" "send-keys -t $pix ':call ChangeBackground()' ENTER"
+  # change neovim
+  for addr in (/opt/homebrew/bin/nvr --serverlist)
+    switch $mode
+      case dark
+        /opt/homebrew/bin/nvr --servername "$addr" -c "set background=dark"
+      case light
+        /opt/homebrew/bin/nvr --servername "$addr" -c "set background=light"
     end
   end
 
