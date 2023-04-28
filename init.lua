eb49e237a05f349f087ea97317da68ada72c3d83
Fatih Arslan <ftharsln@gmail.com>
2023-04-28 20:37:44 +0300

    Add text objects and function jumps
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- init.lua
+++ init.lua
@@ -244,9 +244,62 @@ require("lazy").setup({
     end,
   },
 
-  -- autocompletion
-  --
+  -- Highlight, edit, and navigate code
+  { 
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'go', 'lua', 'ruby', 'vimdoc', 'vim' },
+        indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']]'] = '@function.outer',
+            },
+            goto_next_end = {
+              [']['] = '@function.outer',
+            },
+            goto_previous_start = {
+              ['[['] = '@function.outer',
+            },
+            goto_previous_end = {
+              ['[]'] = '@function.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
+      })
+    end,
+  },
 
+  -- autocompletion
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
