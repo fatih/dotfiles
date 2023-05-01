dc3e9aa21b85c53ab4ee6fd8bbf1801ca18446b9
Fatih Arslan <ftharsln@gmail.com>
2023-05-02 00:35:44 +0300

    Show absolute patah in statusline
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- init.lua
+++ init.lua
@@ -47,7 +47,16 @@ require("lazy").setup({
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
       require("lualine").setup({
-        options = { theme = 'gruvbox' }
+        options = { theme = 'gruvbox' },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+          }
+        }
       })
     end,
   },
