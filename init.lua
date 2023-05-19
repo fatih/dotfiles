f658ad56d0c1415c86a7b8bee65e7cb39de62972
Fatih Arslan <ftharsln@gmail.com>
2023-05-19 10:52:43 +0300

    Use a better formatting setup
    Copied from https://github.com/mrnugget/vimconfig/blob/master/lua/mrnugget/lsp/helpers.lua
    
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- init.lua
+++ init.lua
@@ -723,8 +723,20 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   group = vim.api.nvim_create_augroup('setGoFormatting', { clear = true }),
   pattern = '*.go',
   callback = function()
-    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true, async = false })
-    vim.lsp.buf.format({ async = true })
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 2000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  
+    vim.lsp.buf.format()
   end
 })
 
