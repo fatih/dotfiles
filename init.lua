ea9db2f005bc0dd05f92cee271f4269fc499b490
Fatih Arslan <ftharsln@gmail.com>
2024-11-23 19:19:35 +0300

    add back treesitter
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- init.lua
+++ init.lua
@@ -88,6 +88,106 @@ require("lazy").setup({
     end,
   },
 
+  -- Highlight, edit, and navigate code
+  { 
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'go',
+          'gomod',
+          'proto',
+          'lua',
+          'vimdoc',
+          'vim',
+          'bash',
+          'fish',
+          'json',
+          'markdown',
+          'markdown_inline',
+          'mermaid',
+        },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<space>", -- maps in normal mode to init the node/scope selection with space
+            node_incremental = "<space>", -- increment to the upper named parent
+            node_decremental = "<bs>", -- decrement to the previous node
+            scope_incremental = "<tab>", -- increment to the upper scope (as defined in locals.scm)
+          },
+        },
+        autopairs = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+
+          -- Disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              ["iB"] = "@block.inner",
+              ["aB"] = "@block.outer",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']]'] = '@function.outer',
+            },
+            goto_next_end = {
+              [']['] = '@function.outer',
+            },
+            goto_previous_start = {
+              ['[['] = '@function.outer',
+            },
+            goto_previous_end = {
+              ['[]'] = '@function.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>sn'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>sp'] = '@parameter.inner',
+            },
+          },
+        },
+      })
+    end,
+  },
+
   -- search selection via *
   { 'bronson/vim-visual-star-search' },
 
