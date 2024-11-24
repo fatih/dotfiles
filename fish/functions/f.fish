86bc22f09bfe36d5333d94b479923836a4857000
Fatih Arslan <ftharsln@gmail.com>
2024-11-24 10:42:42 +0300

    Fix f function
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- fish/functions/f.fish
+++ fish/functions/f.fish
@@ -1,3 +1,3 @@
 function f -d "Fuzzy-find and open a file in current directory"
-  nvim (fzf-tmux --height 40% --info inline --border --reverse --preview 'cat {}')
+  nvim (fzf --height 40% --info inline --border --reverse --preview 'cat {}')
 end
