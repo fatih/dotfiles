9f103723f9856f4f8fcbe45aafc85dcb6ff5f978
Fatih Arslan <ftharsln@gmail.com>
2024-12-24 10:31:28 +0300

    Add sticky shifts
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5              ,    KC_6   ,KC_7   ,KC_8     ,KC_9   ,KC_0    ,KC_MINS,
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T              ,    KC_Y   ,KC_U   ,KC_I     ,KC_O   ,KC_P    ,KC_BSLS,
            KC_LCTL,KC_A   ,KC_S   ,KC_D   ,LT(_NUMPAD, KC_F), KC_G   ,    KC_H   ,KC_J   ,KC_K     ,KC_L   ,KC_SCLN ,RCTL_T(KC_QUOT),
-           KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B              ,    KC_N   ,KC_M   ,KC_COMM  ,KC_DOT ,KC_SLSH ,KC_RSFT,
+           OSM(MOD_LSFT)  ,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B              ,    KC_N   ,KC_M   ,KC_COMM  ,KC_DOT ,KC_SLSH ,OSM(MOD_RSFT),
                    KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,                       KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
 
 			             HYPR(KC_7),HYPR(KC_8),    KC_RALT, KC_ESC,
