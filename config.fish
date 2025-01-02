42687b0c1ab36a6c76c5d06ffd4055a5a8a4e642
Fatih Arslan <ftharsln@gmail.com>
2025-01-02 11:02:35 +0300

    Add Atuin
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- config.fish
+++ config.fish
@@ -3,7 +3,6 @@ set -gx GOBIN $HOME/go/bin
 set -gx EDITOR nvim
 set -gx FZF_CTRL_T_COMMAND nvim
 
-
 # git prompt settings
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showdirtystate 'yes'
@@ -21,7 +20,6 @@ set -g __fish_git_prompt_color_branch cyan --dim --italics
 # don't show any greetings
 set fish_greeting ""
 
-
 # don't describe the command for darwin
 # https://github.com/fish-shell/fish-shell/issues/6270
 function __fish_describe_command; end
@@ -34,6 +32,12 @@ set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
 
 # status --is-interactive; and rbenv init - fish | source
+
+set -gx ATUIN_NOBIND "true"
+status --is-interactive; atuin init fish | source
+
+bind \cr _atuin_search
+bind -M insert \cr _atuin_search
 
 # The next line updates PATH for the Google Cloud SDK.
 # if [ -f '/Users/fatih/Code/google-cloud-sdk/path.fish.inc' ]; . '/Users/fatih/Code/google-cloud-sdk/path.fish.inc'; end
