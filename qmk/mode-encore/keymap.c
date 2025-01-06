d1f3514a0c66c9012b4ef9c49ada351f1049e51a
Fatih Arslan <ftharsln@gmail.com>
2025-01-06 15:40:39 +0300

    Add mod-tap to right quote
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- qmk/mode-encore/keymap.c
+++ qmk/mode-encore/keymap.c
@@ -44,30 +44,20 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_MUTE, KC_ESC),
 };
 
-// KC_LCTL      , HOME_A  , HOME_S  , HOME_D  , HOME_F , KC_G  , KC_H   , HOME_J , HOME_K  , HOME_L  , HOME_SCLN  , KC_QUOT      , KC_ENT , KC_VOLD,
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_65_ansi_blocker( /* Base */
-    KC_GRAVE     , KC_1    , KC_2    , TD(TD_3), KC_4   , KC_5  , KC_6   , KC_7   , KC_8    , KC_9    , KC_0       , KC_MINS      , KC_EQL , KC_BSPC, TD(TD_MUTE),
-    KC_TAB       , KC_Q    , KC_W    , KC_E    , KC_R   , KC_T  , KC_Y   , KC_U   , KC_I    , KC_O    , KC_P       , KC_LBRC      , KC_RBRC, KC_BSLS, KC_VOLU,
-    KC_LCTL      , KC_A    , KC_S    , KC_D    , LT(2, KC_F)   , KC_G  , KC_H   , KC_J   , KC_K    , KC_L    , KC_SCLN    , KC_QUOT      , KC_ENT , KC_VOLD,
-    OSM(MOD_LSFT), KC_Z    , KC_X    , KC_C    , KC_V   , KC_B  , KC_N   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH    , OSM(MOD_RSFT), KC_UP  , MACLOCK ,
-    MO(1)        , KC_LALT , KC_LGUI,                             LT(1, KC_SPC),              KC_RGUI , KC_RALT    , KC_LEFT      , KC_DOWN, KC_RGHT
+    KC_GRAVE     , KC_1    , KC_2    , TD(TD_3), KC_4   , KC_5  , KC_6   , KC_7   , KC_8    , KC_9    , KC_0       , KC_MINS         , KC_EQL , KC_BSPC, TD(TD_MUTE),
+    KC_TAB       , KC_Q    , KC_W    , KC_E    , KC_R   , KC_T  , KC_Y   , KC_U   , KC_I    , KC_O    , KC_P       , KC_LBRC         , KC_RBRC, KC_BSLS, KC_VOLU,
+    KC_LCTL      , KC_A    , KC_S    , KC_D    , KC_F   , KC_G  , KC_H   , KC_J   , KC_K    , KC_L    , KC_SCLN    , RCTL_T(KC_QUOT) , KC_ENT , KC_VOLD,
+    OSM(MOD_LSFT), KC_Z    , KC_X    , KC_C    , KC_V   , KC_B  , KC_N   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH    , OSM(MOD_RSFT)   , KC_UP  , MACLOCK ,
+    MO(1)        , KC_LALT , KC_LGUI,                             LT(1, KC_SPC),              KC_RGUI , KC_RALT    , KC_LEFT         , KC_DOWN, KC_RGHT
 ),
 
 [1] = LAYOUT_65_ansi_blocker(
     KC_ESC  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_TRNS, KC_MUTE,
     KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DT_UP  , DT_DOWN, DT_PRNT, KC_VOLU,
     KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
-    KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
-    KC_TRNS , KC_TRNS, KC_TRNS,                            KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS 
-),
-
-[2] = LAYOUT_65_ansi_blocker(
-    KC_TRNS , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_TRNS, KC_TRNS,
-    KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
     KC_TRNS , KC_TRNS, KC_TRNS,                            KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS 
 )
 
