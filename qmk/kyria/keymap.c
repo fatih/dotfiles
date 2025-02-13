d6d198950e7c118213c4dd804a6a799419856651
Fatih Arslan <ftharsln@gmail.com>
2025-02-13 17:20:39 +0300

    Remove from index fingers
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- qmk/kyria/keymap.c
+++ qmk/kyria/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT_split_3x6_5_hlc(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSLS,
-     KC_LCTL , LSFT_T(KC_A) ,  KC_S   ,  KC_D  ,   LT(_NUMPAD, KC_F) ,   LT(_SYMBOLS, KC_G),                           KC_H,   LT(_NUMPAD, KC_J) ,  KC_K ,   KC_L ,RSFT_T(KC_SCLN),CTL_QUOT,
+     KC_LCTL , KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD, KC_F) ,   LSFT_T(KC_G),                           RSFT_T(KC_H),  KC_J ,  KC_K ,   KC_L ,KC_SCLN, CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , OSM(MOD_LSFT),  HYPR(KC_5),     KC_ESC  , OSM(MOD_RSFT), KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 KC_LEFT , KC_RIGHT, KC_LGUI, KC_BSPC , KC_LALT,     KC_TAB    , KC_ENT , LT(_SYMBOLS, KC_SPC), KC_UP, KC_DOWN,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
