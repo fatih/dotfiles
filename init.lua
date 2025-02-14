4e78cb8bd4c7d700942a474d62afce58734d0bd2
Fatih Arslan <ftharsln@gmail.com>
2025-02-14 15:58:59 +0300

    Let's try out snacks.nvim for a while
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- init.lua
+++ init.lua
@@ -284,55 +284,81 @@ require("lazy").setup({
     end
   },
 
-
-  { -- Fuzzy Finder (files, lsp, etc)
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    input = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
     },
-
-    config = function()
-      require('telescope').setup {
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-      vim.keymap.set('n', '<C-b>', builtin.find_files, {})
-      vim.keymap.set('n', '<C-g>', builtin.lsp_document_symbols, {})
-      vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
-      vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
-      vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
-    end,
+    picker = { enabled = true },
+    quickfile = { enabled = true },
   },
+  keys = {
+    -- Top Pickers & Explorer
+    { "<C-b>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<C-n>", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- find
+    { "<C-p>", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<C-r>", function() Snacks.picker.recent() end, desc = "Recent" },
+    { "<C-g>", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" }
+  }
+  },
+
+
+  -- { -- Fuzzy Finder (files, lsp, etc)
+  --   'nvim-telescope/telescope.nvim',
+  --   event = 'VimEnter',
+  --   branch = '0.1.x',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     { -- If encountering errors, see telescope-fzf-native README for installation instructions
+  --       'nvim-telescope/telescope-fzf-native.nvim',
+  --
+  --       -- `build` is used to run some command when the plugin is installed/updated.
+  --       -- This is only run then, not every time Neovim starts up.
+  --       build = 'make',
+  --
+  --       -- `cond` is a condition used to determine whether this plugin should be
+  --       -- installed and loaded.
+  --       cond = function()
+  --         return vim.fn.executable 'make' == 1
+  --       end,
+  --     },
+  --     { 'nvim-telescope/telescope-ui-select.nvim' },
+  --
+  --     -- Useful for getting pretty icons, but requires a Nerd Font.
+  --     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  --   },
+  --
+  --   config = function()
+  --     require('telescope').setup {
+  --       extensions = {
+  --         ['ui-select'] = {
+  --           require('telescope.themes').get_dropdown(),
+  --         },
+  --       },
+  --     }
+  --
+  --     -- Enable Telescope extensions if they are installed
+  --     pcall(require('telescope').load_extension, 'fzf')
+  --     pcall(require('telescope').load_extension, 'ui-select')
+  --
+  --     -- See `:help telescope.builtin`
+  --     local builtin = require 'telescope.builtin'
+  --     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+  --     vim.keymap.set('n', '<C-b>', builtin.find_files, {})
+  --     vim.keymap.set('n', '<C-g>', builtin.lsp_document_symbols, {})
+  --     vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
+  --     vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
+  --     vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
+  --   end,
+  -- },
 
   -- LSP Plugins
   {
