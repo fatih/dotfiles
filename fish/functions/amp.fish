6c4fa7c893b11138cac4d3f4303ca71b142a40c5
Fatih Arslan <ftharsln@gmail.com>
2025-10-16 09:47:15 +0300

    Update amp alias
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- fish/functions/amp.fish
+++ fish/functions/amp.fish
@@ -1,19 +1,17 @@
 function amp
     # Check if we're in a git repository
-    if not git rev-parse --git-dir >/dev/null 2>&1
-        echo "Warning: amp must be called from within a git repository" >&2
-        return 1
+    if git rev-parse --git-dir >/dev/null 2>&1
+        # Get git root directory
+        set git_root (git rev-parse --show-toplevel)
+        set current_dir (pwd)
+        
+        # If not in git root, navigate there
+        if test "$git_root" != "$current_dir"
+            cd "$git_root"
+            echo "Navigated to git root: $git_root"
+        end
     end
     
-    # Check if we're in the git root directory
-    set git_root (git rev-parse --show-toplevel)
-    set current_dir (pwd)
-    
-    if test "$git_root" != "$current_dir"
-        echo "Warning: amp must be called from the git root directory ($git_root)" >&2
-        return 1
-    end
-    
-    # Call the original amp command
-    command amp $argv
+    # Call the original amp command with --ide flag
+    command amp --ide $argv
 end
