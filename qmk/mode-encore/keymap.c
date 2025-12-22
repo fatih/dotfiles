619017c402325949d50dd15b014cde668727af10
Fatih Arslan <ftharsln@gmail.com>
2025-12-22 09:38:54 +0300

    A few more changes
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- qmk/mode-encore/keymap.c
+++ qmk/mode-encore/keymap.c
@@ -17,22 +17,10 @@
 
 #define MACLOCK LGUI(LCTL(KC_Q))
 
-// Left-hand home row mods
-#define HOME_A LCTL_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LGUI_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
-
-// Right-hand home row mods
-#define HOME_J RSFT_T(KC_J)
-#define HOME_K RGUI_T(KC_K)
-#define HOME_L RALT_T(KC_L)
-#define HOME_SCLN RCTL_T(KC_SCLN)
-
 // Tap Dance declarations
 enum {
     TD_9,
-    TD_MUTE,
+    TD_LOCK,
 };
 
 // Tap Dance definitions
@@ -40,17 +28,17 @@ tap_dance_action_t tap_dance_actions[] = {
     // Tap once for 9, twice for CMD + SHIFT + 3 (Screenshot)
     [TD_9] = ACTION_TAP_DANCE_DOUBLE(KC_9, LSG(KC_3)),
 
-    // Tap once for ESC twice for mute
-    [TD_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_MUTE),
+    // Tap once for Mute twice for Mac lock
+    [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(KC_MUTE, MACLOCK),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_65_ansi_blocker( /* Base */
-    KC_GRAVE     , KC_1    , KC_2    , KC_3    , KC_4   , KC_5  , KC_6   , KC_7   , KC_8    , TD(TD_9), KC_0       , KC_MINS         , KC_EQL , KC_BSPC, KC_ESC,
+    KC_GRAVE     , KC_1    , KC_2    , KC_3    , KC_4   , KC_5  , KC_6   , KC_7   , KC_8    , KC_9    , KC_0       , KC_MINS         , KC_EQL , KC_BSPC, KC_ESC,
     KC_TAB       , KC_Q    , KC_W    , KC_E    , KC_R   , KC_T  , KC_Y   , KC_U   , KC_I    , KC_O    , KC_P       , KC_LBRC         , KC_RBRC, KC_BSLS, KC_VOLU,
     KC_LCTL      , KC_A    , KC_S    , KC_D    , KC_F   , KC_G  , KC_H   , KC_J   , KC_K    , KC_L    , KC_SCLN    , RCTL_T(KC_QUOT) ,          KC_ENT , KC_VOLD,
-    OSM(MOD_LSFT), KC_Z    , KC_X    , KC_C    , KC_V   , KC_B  , KC_N   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH    , OSM(MOD_RSFT)   ,          KC_UP  , KC_MUTE,
-    MACLOCK      , KC_LALT , KC_LGUI,                             LT(1, KC_SPC),              KC_RGUI , KC_RALT    ,                  KC_LEFT , KC_DOWN, KC_RGHT
+    OSM(MOD_LSFT), KC_Z    , KC_X    , KC_C    , KC_V   , KC_B  , KC_N   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH    , OSM(MOD_RSFT)   ,          KC_UP  , MACLOCK,
+    LSG(KC_3)    , KC_LALT , KC_LGUI,                             LT(1, KC_SPC),              KC_RGUI , KC_RALT    ,                  KC_LEFT , KC_DOWN, KC_RGHT
 ),
 
 [1] = LAYOUT_65_ansi_blocker(
